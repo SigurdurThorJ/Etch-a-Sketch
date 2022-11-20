bd654092a633a577c8da52ae02fc7a919d373e78
Sigurdur Thor <sigurdurtj@gmail.com>
2022-11-20 17:31:55 +0000

    added grid to  JS and eventListener for hvoer effect, Also some style to CSS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+
+I am going to build a browser version of a sketchpad or Etch-a-Sketch. I will have to use DOM manipulation in this project.
---
+++ script.js
@@ -0,0 +1,28 @@
+const sizeOfGrid = 16;
+const container = document.querySelector(".container");
+
+const createGrid = (amtOfGrid) => {
+    for (let i = 0; i < amtOfGrid; i++) {
+        const row = document.createElement('div')
+        row.classList.add('grid-row')
+
+        for(let j = 0; j < amtOfGrid; j++) {
+            const widthAndHeight = 960 / sizeOfGrid;
+            const gridBox = document.createElement('div')
+            gridBox.classList.add('grid-box')
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
+
+            gridBox.addEventListener('mouseenter', () => {
+                gridBox.style.backgroundColor = 'black'
+            })
+            row.appendChild(gridBox)
+        }
+
+        container.appendChild(row)
+    }
+}
+
+
+
+createGrid(sizeOfGrid);
